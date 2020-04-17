6a5d4052d11b960aa20728edf743267075c83fbb
Vera Tretyakova <vera.d.tretyakova@gmail.com>
2020-04-17 18:06:31 +0300

    Update cleaning_1_subject_ICA.py

--- 1_subject/cleaning_1_subject_ICA.py
+++ 1_subject/cleaning_1_subject_ICA.py
@@ -18,9 +18,21 @@ picks_meg = mne.pick_types(raw.info, meg=True, eeg=False, eog=False,
                            stim=False, exclude=[])
 raw.filter(1., 50., fir_design='firwin')
 
+'''
+to select number components , we calculate the rank
+'''
+rank = mne.compute_rank(raw, rank='info')
+rank = int(rank['meg'])
+
+'''
+select events for horisontal and vertical eyes movements
+'''
+eog_events_h = mne.preprocessing.find_eog_events(raw, event_id=998, ch_name='EOG062') #horisontal
+eog_events_v = mne.preprocessing.find_eog_events(raw, event_id=997, ch_name='EOG061') #vertical
+
 #ICA
-n_components = 25  # if float, select n_components by explained variance of PCA
-method = 'fastica'  # for comparison with EEGLAB try "extended-infomax" here
+n_components = rank  
+method = 'fastica' 
 decim = 3  # we need sufficient statistics, not all time points -> saves time
 random_state = 23
 
@@ -36,29 +48,42 @@ for id, i in enumerate(ica_comp_L005):
   i.savefig('/net/server/data/home/vtretyakova/Desktop/New_experiment/ica_comp_{0}_L001_d1p1.jpeg'.format(comp)) 
 
 #EOG
-eog_epochs = create_eog_epochs(raw, reject=reject)  # get single EOG trials
-eog_inds, scores = ica.find_bads_eog(eog_epochs)  # find via correlation
-print(ica.labels_)
-eog_average = create_eog_epochs(raw, reject=reject,
-                                picks=picks_meg).average()
-
-score = ica.plot_scores(scores, exclude=eog_inds);  # look at r scores of components
+#horizontal eyes movements
+eog_epochs_h = create_eog_epochs(raw, ch_name='EOG062', event_id=998, reject=reject)  # get single EOG trials
+eog_inds_h, scores_h = ica.find_bads_eog(eog_epochs_h, ch_name='EOG062', threshold=5.0)  # find via correlation
+score_eog_h = ica.plot_scores(scores_h, exclude=eog_inds_h); 
 # we can see that only one component is highly correlated and that this
 # component got detected by our correlation analysis (red).
-score.savefig('/net/server/data/home/vtretyakova/Desktop/New_experiment/ica_score_L001_d1p1.jpeg')
+score.savefig('home/..../Desktop/New_experiment/ica_score_h_eog_L001_d1p1.jpeg')
+
+properties_h = ica.plot_properties(eog_epochs_h, picks=eog_inds_h, psd_args={'fmax': 35.}, image_args={'sigma': 1.})
+for id, p in enumerate(properties_h):
+  p.savefig('home/..../Desktop/New_experiment/ica_prop_v_{0}_L001_d1p1.jpeg'.format(id)) 
+
+eog_average_h = eog_epochs_h.average()
+overlay_h = ica.plot_overlay(eog_average_h, exclude=eog_inds_h, show=False);
+overlay_h.savefig('/home/....../Desktop/New_experiment/ica_overlay_h_L001_d1p1.jpeg')
+
+#vertical eyes movements
+eog_epochs_v = create_eog_epochs(raw, ch_name='EOG061', event_id=997, reject=reject)  # get single EOG trials
+eog_inds_v, scores_v = ica.find_bads_eog(eog_epochs_v, ch_name='EOG061', threshold=5.0)  # find via correlation
+score_eog_v = ica.plot_scores(scores_v, exclude=eog_inds_v); 
+# we can see that only one component is highly correlated and that this
+# component got detected by our correlation analysis (red).
+score.savefig('home/..../Desktop/New_experiment/ica_score_v_eog_L001_d1p1.jpeg')
+
+properties_v = ica.plot_properties(eog_epochs_v, picks=eog_inds_v, psd_args={'fmax': 35.}, image_args={'sigma': 1.})
+for id, p in enumerate(properties_v):
+  p.savefig('home/..../Desktop/New_experiment/ica_prop_v_{0}_L001_d1p1.jpeg'.format(id)) 
+
+eog_average_v = eog_epochs_v.average()
+overlay_v = ica.plot_overlay(eog_average_v, exclude=eog_inds_v, show=False);
+overlay_v.savefig('/home/....../Desktop/New_experiment/ica_overlay_v_L001_d1p1.jpeg')
+
 
 #Note in MNE 20.0 there is no exclude argument in ica.plot_sources
-sources = ica.plot_sources(eog_average, exclude=eog_inds);  # look at source time course
-sources.savefig('/net/server/data/home/vtretyakova/Desktop/New_experiment/ica_sources_L001_d1p1.jpeg')
-
-properties = ica.plot_properties(eog_epochs, picks=eog_inds, psd_args={'fmax': 35.},
-                    image_args={'sigma': 1.})
-
-for id, p in enumerate(properties):
-  p.savefig('/net/server/data/home/vtretyakova/Desktop/New_experiment/ica_prop_{0}_L001_d1p1.jpeg'.format(id)) 
-  
-overlay = ica.plot_overlay(eog_average, exclude=eog_inds, show=False);
-overlay.savefig('/net/server/data/home/vtretyakova/Desktop/New_experiment/ica_overlay_L001_d1p1.jpeg')
+sources = ica.plot_sources(eog_average_v, exclude=eog_inds-V);  # look at source time course
+sources.savefig('/home/........../Desktop/New_experiment/ica_sources-v_L001_d1p1.jpeg')
 
 #ECG
 ecg_epochs = create_ecg_epochs(raw, tmin=-.5, tmax=.5, reject=reject)
